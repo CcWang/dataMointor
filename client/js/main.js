f37fce2565cf244def4fb8e653e4d2b638a0e9fb
Chongchong Wang <chongchong1110@gmail.com>
2016-09-23 23:51:36 -0700

    get data from db

--- client/js/main.js
+++ client/js/main.js
@@ -1,38 +1,37 @@
 $(document).ready(function(){
   // start highcharts
+function bindData(serName){
+    $.post(serName,function(data){
+      return data['y'];
+    })
+  };
   $('#container').highcharts({
     chart:{
       type:'area',
-      events:{
-        load:function(){
-          var chart=this;
-          var s1=this.series[0];
-          var s2=this.series[1];
-          var s3=this.series[2];
-          var x=125
-          console.log(s1,s2,s3);
-          setInterval(function(){
-            var y1=Math.random()*98+Math.random()*50;
-            s1.addPoint([x,y1],false,true);
-            s2.addPoint([x,y1],false,true);
-            s3.addPoint([x,y1],false,true);
-            chart.redraw();
-            x+=1;
-          },1000)
-        }
-      }
+    //   events:{
+    //     load:function(){
+    //       var chart=this;
+    //       var s1=this.series[0];
+    //       var s2=this.series[1];
+    //       var s3=this.series[2];
+    //       var x=125
+    //       console.log(s1,s2,s3);
+    //       setInterval(function(){
+    //         var y1=Math.random()*98+Math.random()*50;
+    //         s1.addPoint([x,y1],false,true);
+    //         s2.addPoint([x,y1],false,true);
+    //         s3.addPoint([x,y1],false,true);
+    //         chart.redraw();
+    //         x+=1;
+    //       },1000)
+    //     }
+    //   }
     },
     title:{
       text:'time stack'
     },
     xAxis:{
-      // categories:(function(){
-      //   var data=[];
-      //   for(var i=1;i<62;i+=9){
-      //     data.push(i);
-      //   };
-      //   return data;
-      // }()),
+      type:'datetime',
       tickmarkPlacement:'on'
     },
     yAxis:{
@@ -47,33 +46,15 @@ $(document).ready(function(){
     },
     series: [{
             name: 'texting one',
-            data: (function(){
-              var data=[];
-              for (var i=100;i<125;i++){
-                data.push([i,(Math.random()*100+50)]);
-              }
-              return data
-            })()
-
-        }, {
-            name: 'testing two',
-            data: (function(){
-              var data=[];
-              for (var i=100;i<125;i++){
-                data.push([i,(Math.random()*100+50)]);
-              }
-              return data;
-            })()
+            data: []
         },{
-          name:'testing three',
-          data:(function () {
-            var data=[];
-            for (var i=100;i<125;i++){
-              data.push([i,(Math.random()*100+50)]);
-            }
-            return data
-          })()
+            name:'texting two',
+            data:[]
+        },{
+            name:'texting three',
+            data:[]
         }],
+
 
     credits:{
       enabled:false
@@ -95,19 +76,21 @@ $(document).ready(function(){
   // end of highcharts
 
   // get data from SQL
-  $.post('/sqltime',function(data){
+  $.post('/dataOne',function(data){
     var chart = $('#container').highcharts();
-    // chart.series[0].update({
-		//  		data:dates['date']
-		//  });
-    //  chart.series[1].update({
-    //     data:dates['hours']
-    //  });
-    //  chart.series[2].update({
-    //     data:dates['mins']
-    //  });
-    //  chart.series[3].update({
-    //     data:dates['secs']
-    //  });
+    console.log(data['timestamp'])
+    chart.series[0].update({
+      data:data['one']
+    });   
+    chart.series[1].update({
+      data:data['two']
+    });
+    chart.series[2].update({
+      data:data['three']
+    });
+    chart.xAxis[0].setCategories(
+      data['timestamp']
+    )
   });
+
 })
--- server.js
+++ server.js
@@ -32,16 +32,24 @@ connection.connect(function(err){
   console.log('connecd as id'+ connection.threadId);
 })
 // route and query
-app.get('/users',function(req,res){
-  connection.query('select * from times',function(err,rows){
-    res.render('index',{users:rows});
-  })
 
-});
-app.post('/sqltime',function(req,res){
-  connection.query('select * from time',function(err,rows){
-    // console.log(rows)
-    res.send(rows);
+app.post('/dataOne',function(req,res){
+  connection.query('select timeStamp,data,metadata,threedata from serverOne',function(err,rows){
+    var data = {
+      'one':[],
+      'two':[],
+      'three':[],
+      'timestamp':[]
+    };
+    for(var i=0;i<rows.length;i++){
+      // console.log(rows[i]["data"])
+      data['one'].push(rows[i]["data"]);
+      data['two'].push(rows[i]['metadata']);
+      data['three'].push(rows[i]['threedata']);
+      data['timestamp'].push(rows[i]["timeStamp"]);
+
+    }
+    res.send(data);
   })
 })
 
@@ -53,37 +61,16 @@ app.post('/sqltime',function(req,res){
   var y = Math.random()*1000;
   console.log ('time Now',timeNow)
 
-  var queryOne = "insert into serverOne (timestamp,data) VALUES (FROM_UNIXTIME("+timeNow+"),"+Math.random()*1000+")";
-  var queryTwo = "insert into serverTwo (timestamp,data) VALUES (FROM_UNIXTIME("+timeNow+"),"+Math.random()*1000+")";
-  var queryThree = "insert into serverThree (timestamp,data) VALUES (FROM_UNIXTIME("+timeNow+"),"+Math.random()*1000+")";
+  var queryOne = "insert into serverOne (timestamp,data, metadata,threedata) VALUES (FROM_UNIXTIME("+timeNow+",'%Y-%m-%d %H:%M:%S'),"+Math.random()*1000+","+Math.random()*1000+","+Math.random()*1000+")";
+  
   connection.query(queryOne,function(err,rows){
     if(err){
       console.log(err)
     }
   });
-  connection.query(queryTwo,function(err,rows){
-    if(err){
-      console.log(err)
-    }
-  });
-  connection.query(queryThree,function(err,rows){
-    if(err){
-      console.log(err)
-    }
-  });
-};
+};  
 
- function dataThree (){
-  var timeNow = new Date().getTime();
-  console.log ('three',timeNow)
-  var timing = {'timestamp':timeNow,'data':Math.random()*1000};
-  connection.query('insert into serverThree SET ? ',timing,function(err,rows){
-    if(err){
-      console.log(err)
-    }
-  });
-};
-setInterval(function(){
-  dataOne();
+// setInterval(function(){
+//   dataOne();
 
-},3600)
+// },3600)
